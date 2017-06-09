cc92797d1d199ac1cd88bd7f479e4a6f1e567e04
Suyash <suyashgarg2@gmail.com>
2017-06-09 12:21:40 +0530

    Main App,first commit

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils import timezone
+class Appreq(models.Model):
+    ename = models.ForeignKey('auth.User')
+    mail = models.EmailField()
+    cname = models.CharField(max_length=50)
+    phone = models.BigIntegerField()
+    purpose = models.TextField()
+    title = models.CharField(max_length=50)
+    start_date = models.DateTimeField(
+        default=timezone.now)
+    end_date = models.DateTimeField(
+        default=timezone.now)
+    def submit(self):
+        self.save()
+    def __str__(self):
+        return self.title
+    
