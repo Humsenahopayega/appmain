bfba1bf81ba35946e72aaa951442052e981baa3f
Suyash <suyashgarg2@gmail.com>
2017-06-21 13:17:15 +0530

    asa

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.appreq, name='appreq'),
     url(r'^registration$', views.users, name='users'),
+    url(r'^cal$', views.this_month, name='this_month'),
 ]
--- app/views.py
+++ app/views.py
@@ -8,7 +8,7 @@ from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 import subprocess
 from calendar import monthrange
-
+from datetime import datetime
 @csrf_exempt
 def appreq(request):
     if request.method == 'POST':
@@ -30,52 +30,58 @@ def users(request):
     form = RegForm()
     return render_to_response( 'app/register.html', {'regform':form}, RequestContext(request))
 
-def named_month(pMonthNumber):
-    """
-    Return the name of the month, given the month number
-    """
-    return date(1900, pMonthNumber, 1).strftime('%B')
+def named_month(month_number):
 
-def home(request):
-    """
-    Show calendar of events this month
-    """
-    lToday = datetime.now()
-    return calendar(request, lToday.year, lToday.month)
+	"""
+	Return the name of the month, given the number.
+	"""
+	return datetime(1900, month_number, 1).strftime("%B")
 
-def calendar(request, pYear, pMonth):
-    """
-    Show calendar of events for specified month and year
-    """
-    lYear = int(pYear)
-    lMonth = int(pMonth)
-    lCalendarFromMonth = datetime(lYear, lMonth, 1)
-    lCalendarToMonth = datetime(lYear, lMonth, monthrange(lYear, lMonth)[1])
-    lContestEvents = ContestEvent.objects.filter(date_of_event__gte=lCalendarFromMonth, date_of_event__lte=lCalendarToMonth)
-    lCalendar = ContestCalendar(lContestEvents).formatmonth(lYear, lMonth)
-    lPreviousYear = lYear
-    lPreviousMonth = lMonth - 1
-    if lPreviousMonth == 0:
-        lPreviousMonth = 12
-        lPreviousYear = lYear - 1
-    lNextYear = lYear
-    lNextMonth = lMonth + 1
-    if lNextMonth == 13:
-        lNextMonth = 1
-        lNextYear = lYear + 1
-    lYearAfterThis = lYear + 1
-    lYearBeforeThis = lYear - 1
+def this_month(request):
+	"""
+	Show calendar of readings this month.
+	"""
+	today = datetime.now()
+	return calendar(request, today.year, today.month)
 
-    return render_auth(request, 'calendar/home.html', {'Calendar' : mark_safe(lCalendar),
-                                                       'Month' : lMonth,
-                                                       'MonthName' : named_month(lMonth),
-                                                       'Year' : lYear,
-                                                       'PreviousMonth' : lPreviousMonth,
-                                                       'PreviousMonthName' : named_month(lPreviousMonth),
-                                                       'PreviousYear' : lPreviousYear,
-                                                       'NextMonth' : lNextMonth,
-                                                       'NextMonthName' : named_month(lNextMonth),
-                                                       'NextYear' : lNextYear,
-                                                       'YearBeforeThis' : lYearBeforeThis,
-                                                       'YearAfterThis' : lYearAfterThis,
-                                                   })
+def calendar(request, year, month, series_id=None):
+	"""
+	Show calendar of readings for a given month of a given year.
+	``series_id``
+	The reading series to show. None shows all reading series.
+	"""
+	my_year = int(year)
+	my_month = int(month)
+	my_calendar_from_month = datetime(my_year, my_month, 1)
+	my_calendar_to_month = datetime(my_year, my_month, monthrange(my_year, my_month)[1])
+
+	my_reading_events = Appreq.objects.filter(start_date=my_calendar_from_month).filter(end_date=my_calendar_to_month)
+	if series_id:
+		my_reading_events = my_reading_events.filter(series=series_id)
+
+	# Calculate values for the calendar controls. 1-indexed (Jan = 1)
+	my_previous_year = my_year
+	my_previous_month = my_month - 1
+	if my_previous_month == 0:
+		my_previous_year = my_year - 1
+		my_previous_month = 12
+	my_next_year = my_year
+	my_next_month = my_month + 1
+	if my_next_month == 13:
+		my_next_year = my_year + 1
+		my_next_month = 1
+	my_year_after_this = my_year + 1
+	my_year_before_this = my_year - 1
+	return render_to_response("cal_template.html", { 'readings_list': my_reading_events,
+														'month': my_month,
+														'month_name': named_month(my_month),
+														'year': my_year,
+														'previous_month': my_previous_month,
+														'previous_month_name': named_month(my_previous_month),
+														'previous_year': my_previous_year,
+														'next_month': my_next_month,
+														'next_month_name': named_month(my_next_month),
+														'next_year': my_next_year,
+														'year_before_this': my_year_before_this,
+														'year_after_this': my_year_after_this,
+	}, RequestContext(request))
