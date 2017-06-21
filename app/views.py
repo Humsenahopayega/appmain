340579536faf21796bc969d607ba23ce65a92ebb
Suyash <suyashgarg2@gmail.com>
2017-06-21 10:30:01 +0530

    No idea what's happening

--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ from .forms import RegForm
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 import subprocess
+from calendar import monthrange
 
 @csrf_exempt
 def appreq(request):
@@ -28,3 +29,53 @@ def users(request):
           reg.save()
     form = RegForm()
     return render_to_response( 'app/register.html', {'regform':form}, RequestContext(request))
+
+def named_month(pMonthNumber):
+    """
+    Return the name of the month, given the month number
+    """
+    return date(1900, pMonthNumber, 1).strftime('%B')
+
+def home(request):
+    """
+    Show calendar of events this month
+    """
+    lToday = datetime.now()
+    return calendar(request, lToday.year, lToday.month)
+
+def calendar(request, pYear, pMonth):
+    """
+    Show calendar of events for specified month and year
+    """
+    lYear = int(pYear)
+    lMonth = int(pMonth)
+    lCalendarFromMonth = datetime(lYear, lMonth, 1)
+    lCalendarToMonth = datetime(lYear, lMonth, monthrange(lYear, lMonth)[1])
+    lContestEvents = ContestEvent.objects.filter(date_of_event__gte=lCalendarFromMonth, date_of_event__lte=lCalendarToMonth)
+    lCalendar = ContestCalendar(lContestEvents).formatmonth(lYear, lMonth)
+    lPreviousYear = lYear
+    lPreviousMonth = lMonth - 1
+    if lPreviousMonth == 0:
+        lPreviousMonth = 12
+        lPreviousYear = lYear - 1
+    lNextYear = lYear
+    lNextMonth = lMonth + 1
+    if lNextMonth == 13:
+        lNextMonth = 1
+        lNextYear = lYear + 1
+    lYearAfterThis = lYear + 1
+    lYearBeforeThis = lYear - 1
+
+    return render_auth(request, 'calendar/home.html', {'Calendar' : mark_safe(lCalendar),
+                                                       'Month' : lMonth,
+                                                       'MonthName' : named_month(lMonth),
+                                                       'Year' : lYear,
+                                                       'PreviousMonth' : lPreviousMonth,
+                                                       'PreviousMonthName' : named_month(lPreviousMonth),
+                                                       'PreviousYear' : lPreviousYear,
+                                                       'NextMonth' : lNextMonth,
+                                                       'NextMonthName' : named_month(lNextMonth),
+                                                       'NextYear' : lNextYear,
+                                                       'YearBeforeThis' : lYearBeforeThis,
+                                                       'YearAfterThis' : lYearAfterThis,
+                                                   })
--- test3.py
+++ test3.py
@@ -7,6 +7,7 @@ from oauth2client import tools
 from oauth2client.file import Storage
 from app.models import Appreq
 import datetime
+import json
 
 try:
     import argparse
@@ -20,7 +21,7 @@ APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
 
 def get_credentials():
-    
+
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
     if not os.path.exists(credential_dir):
@@ -35,7 +36,7 @@ def get_credentials():
         flow.user_agent = APPLICATION_NAME
         if flags:
             credentials = tools.run_flow(flow, store, flags)
-        else: 
+        else:
             credentials = tools.run(flow, store)
             print('Storing credentials to ' + credential_path)
     return credentials
@@ -47,10 +48,10 @@ def main() :
         service = discovery.build('calendar' , 'v3' , http=http)
         summary = Appreq.objects.filter( title='Dinner').values('title')
         EVENT = {
-		'summary': summary,
+		        'summary': summary["title"],
                 'description': 'A chance to hear more about Google\'s developer products.',
                 'start': {'dateTime': '2017-06-15T09:00:00+05:30'},
-                'end': {'dateTime': '2017-06-15T17:00:00+05:30'}
+                'end': {'dateTime': '2017-06-15T17:00:00+05:30'},
 		}
         event = service.events().insert(body=EVENT,calendarId='primary',sendNotifications=True).execute()
         print ('Event created: %s' % (event.get('htmlLink')))
